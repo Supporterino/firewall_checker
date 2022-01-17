b7e9b77761ccdfc46d2a16354831768c8c649829
Supporterino <lars@roth-kl.de>
2022-01-17 19:49:13 +0100

    Add rule classes

--- src/rules/targeted_rule.ts
+++ src/rules/targeted_rule.ts
@@ -1,15 +1,15 @@
-import { Protocol, Rule, RuleType } from ".";
+import { Protocol, Rule, RuleType } from '.';
 
 export class TargetedIPRule extends Rule {
-    private __targetedIP: string;
-    public get targetedIP(): string {
-        return this.__targetedIP;
-    }
-    public set targetedIP(value: string) {
-        this.__targetedIP = value;
-    }
-    constructor(port: number, proto: Protocol, comment: string, type: RuleType, target: string, targetIP: string) {
-        super(port, proto, comment, type, target)
-        this.__targetedIP = targetIP
-    }
+  private __targetedIP: string;
+  public get targetedIP(): string {
+    return this.__targetedIP;
+  }
+  public set targetedIP(value: string) {
+    this.__targetedIP = value;
+  }
+  constructor(port: number, proto: Protocol, comment: string, type: RuleType, target: string, targetIP: string) {
+    super(port, proto, comment, type, target);
+    this.__targetedIP = targetIP;
+  }
 }
