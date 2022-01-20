36f97c60658fd4e3eb7622b2cb53d71e09cf075f
Supporterino <lars@roth-kl.de>
2022-01-20 19:33:49 +0100

    Cleanup logger

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,27 +1,9 @@
-import { appendFileSync } from 'fs';
-import { join } from 'path';
-import { ILogObject, Logger, TLogLevelName } from 'tslog';
+import { Logger, TLogLevelName } from 'tslog';
 
-const loglevel = process.env.LOG_LEVEL || 'debug';
-const writeLogFile = (logObject: ILogObject): void => {
-  // appendFileSync(join(process.cwd(), 'logs/main.log'), logObject + '\n');
-};
+const loglevel = process.env.LOG_LEVEL || 'silly';
 
 export const logger = new Logger({
   name: 'firewall_checker',
   minLevel: loglevel as TLogLevelName,
   dateTimeTimezone: 'Europe/Berlin'
 });
-
-logger.attachTransport(
-  {
-    silly: writeLogFile,
-    debug: writeLogFile,
-    trace: writeLogFile,
-    info: writeLogFile,
-    warn: writeLogFile,
-    error: writeLogFile,
-    fatal: writeLogFile
-  },
-  'debug'
-);
