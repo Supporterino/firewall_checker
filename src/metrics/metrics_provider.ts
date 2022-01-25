4e602378308a7b5c9b953494344950c5db223e47
Supporterino <lars@roth-kl.de>
2022-01-25 19:00:30 +0100

    `metrics` docs

--- src/metrics/metrics_provider.ts
+++ src/metrics/metrics_provider.ts
@@ -5,6 +5,9 @@ import { hostname } from 'os';
 import { getExpectedAsString, PortCheck, RunResult } from '../port_check';
 import { router } from '.';
 
+/**
+ * This class provides the webserver for the metrics, docs and api endpoint and is reponsible for creating the metrics from the ran `PortCheck`s
+ */
 export class MetricsProvider implements Provider {
   private __app: express.Application;
   private __port: number;
@@ -28,12 +31,20 @@ export class MetricsProvider implements Provider {
     });
   }
 
+  /**
+   * Clears the metrics array and increases the update counter and sets the new last updated time
+   */
   update(): void {
     this.__metrics = new Array<string>();
     this.__updateCounter++;
     this.addMetric('port_check_update_count', this.__updateCounter, [{ last_update: new Date().toString() }]);
   }
 
+  /**
+   * Build the metric from an ran `PortCheck`
+   * @param result the `RunResult`
+   * @param check the `PortCheck` which was ran
+   */
   receiveMetric(result: RunResult, check: PortCheck): void {
     switch (result) {
       case RunResult.EXPECTED:
@@ -50,7 +61,13 @@ export class MetricsProvider implements Provider {
     }
   }
 
-  addMetric(name: string, value: number, labels?: Array<any>): void {
+  /**
+   * Build a metric string from the given parameters and add it to the metrics array
+   * @param name the name of the metric
+   * @param value the value of the metric
+   * @param labels the additional labels for the metric as an array. [{ label_name: labek_value }]
+   */
+  private addMetric(name: string, value: number, labels?: Array<any>): void {
     if (!labels) labels = [];
 
     if (process.env.NODE_ENV === 'production') labels.push({ host: process.env.HOSTNAME });
@@ -68,6 +85,9 @@ export class MetricsProvider implements Provider {
     this.__metrics.push(`${name}{${label_string}} ${value}`);
   }
 
+  /**
+   * Method not implemented.
+   */
   stats(): string {
     throw new Error('Method not implemented.');
   }
