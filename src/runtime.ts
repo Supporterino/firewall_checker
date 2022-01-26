546dbd92c2c2e6e4ca34551427952f76e8cf8b19
Lars <lars@roth-kl.de>
2022-01-26 07:56:08 +0100

    docs :D

--- src/runtime.ts
+++ src/runtime.ts
@@ -1,13 +1,21 @@
 import { AsyncTask, SimpleIntervalJob, Task, ToadScheduler } from 'toad-scheduler';
 import { inventory_provider, rule_provider, port_check_provider, metrics_provider, git_updater, executor } from '.';
 
+/**
+ * This class wrappes the normal program flow and creates the scheduler to realise this flow
+ */
 export class Runtime {
   private __scheduler: ToadScheduler;
   constructor() {
     this.__scheduler = new ToadScheduler();
   }
 
-  start() {
+  /**
+   * This function starts the program flow by first executing the checkout of the repo and update of each component.
+   * Then a schedueler is created which checks for changes on the remote every 2 hours and if there are changes component
+   * updates are triggered. It also registers a task which runs the checks every 20 mins
+   */
+  start(): void {
     git_updater.cloneRepo();
     inventory_provider.update();
     rule_provider.update();
